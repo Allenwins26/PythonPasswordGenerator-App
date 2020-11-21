fa144045dea5c5fb435a2a325fc86409b6a532ff
Allen <72219631+Allenwins26@users.noreply.github.com>
2020-11-21 11:33:47 -0500

    Update PythonPasswordGenerator-App.py

--- PythonPasswordGenerator-App.py
+++ PythonPasswordGenerator-App.py
@@ -1,1 +1,43 @@
+# importing the tkinter module
+from tkinter import *
 
+# importing the pyperclip module to use it to copy our generated 
+# password to clipboard
+import pyperclip
+
+# random module will be used in generating the random password
+import random
+
+# initializing the tkinter
+root = Tk()
+
+# setting the width and height of the gui
+root.geometry("400x400")    # x is small case here
+
+# declaring a variable of string type and this variable will be 
+# used to store the password generated
+passstr = StringVar()
+
+# declaring a variable of integer type which will be used to 
+# store the length of the password entered by the user
+passlen = IntVar()
+
+# setting the length of the password to zero initially
+passlen.set(0)
+
+
+# function to generate the password
+def generate():
+    # storing the keys in a list which will be used to generate 
+    # the password 
+    pass1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+            'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+            'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+            'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 
+            'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', 
+            '9', '0', ' ', '!', '@', '#', '$', '%', '^', '&', 
+            '*', '(', ')']
+
+    # declaring the empty string
+    password = ""
